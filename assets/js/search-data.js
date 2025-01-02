8a5605e4a9992e08443b4fa1a04e644c1328cfb6
nicolas-alexander-weiss <nicolas.alexander.weiss@gmail.com>
2025-01-02 14:56:17 +0000

    Deploying to gh-pages from @ nicolas-alexander-weiss/nicolas-alexander-weiss.github.io@602b6e584a146cd7e91bb932bc42c0ad4721688a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-i-am-starting-a-phd-position-at-the-mpi-mis-leipzig-sparkles",
+          title: 'I am starting a PhD position at the MPI MiS, Leipzig! :sparkles:.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
